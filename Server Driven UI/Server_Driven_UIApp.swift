903075017ff2e22e9285041addab1b47347ef750
narayanshettigar <narayanshettigar16@gmail.com>
2024-07-20 21:18:01 +0530

    Initial Commit

---
+++ Server Driven UI/Server_Driven_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Server_Driven_UIApp.swift
+//  Server Driven UI
+//
+//  Created by Narayan Shettigar on 20/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Server_Driven_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Server Driven UIUITests/Server_Driven_UIUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Server_Driven_UIUITestsLaunchTests.swift
+//  Server Driven UIUITests
+//
+//  Created by Narayan Shettigar on 20/07/24.
+//
+
+import XCTest
+
+final class Server_Driven_UIUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
